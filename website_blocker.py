9c6aae766516183ea4d9461b4527c7beb5421ef0
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-16 13:55:17 +0000

    9-135 Check whether in working hours, using datetime.datetime

--- website_blocker.py
+++ website_blocker.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime as dt     # shortened name as expressions could be fairly long
 
 hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 # r prefix for a string literal, to avoid Win path slash separators as escape characters
@@ -6,7 +7,14 @@ hosts_path = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 website_list = ['www.facebook.com', 'facebook.com']
 
+working_hours_start = 8     # 0800 hours
+working_hours_finish = 18   # 1800 hours
+
 while True:
-    print(1)        # just to illustrate working loop
+    if dt(dt.now().year, dt.now().month, dt.now().day, working_hours_start) < dt.now() \
+            < dt(dt.now().year, dt.now().month, dt.now().day, working_hours_finish):
+        # is time between working time start and working time finish?
+        print('In working hours')        # just to illustrate working loop
+    else:
+        print('Not in working hours')
     time.sleep(5)
-# is time between working time start and working time finish?
